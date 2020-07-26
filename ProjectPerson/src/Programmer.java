abebe40b2229dc419119dff2a4bb02e2336d1095
Maria Salmasian <maria.salmasian@gmail.com>
2020-07-27 00:24:31 +0400

    -added comments -walk() is no longer an abstract method

--- ProjectPerson/src/Programmer.java
+++ ProjectPerson/src/Programmer.java
@@ -1,35 +1,56 @@
 public class Programmer extends Person {
     private String companyName;
 
+    /**
+     * constructor for programmer
+     * @param name
+     * @param designation
+     * @param companyName
+     */
     protected Programmer(String name, String designation, String companyName) {
         super(name, designation);
         this.companyName = companyName;
     }
 
+
+    /**
+     * returns the value of companyName
+     * @return
+     */
     private String getCompanyName() {
         return companyName;
     }
 
+
+    /**
+     * prints the name of the person and where he is working as a developer
+     */
     private void coding() {
         System.out.println(getName() + " is a senior developer , at " + getCompanyName());
     }
 
-    @Override
-    protected void walk(int steps) {
-        setStep(steps);
-        System.out.println(getName() + " walks " + steps + " steps a day");
-    }
 
+    /**
+     * overrides the method eat from person, prints what this person eats
+     */
     @Override
     protected void eat() {
         System.out.println(getName() + " eats everything, but hates breakfasts");
     }
 
+
+    /**
+     * overrides the method learn() from person and prints the education of the person as a programmer
+     */
     @Override
     protected void learn() {
         System.out.println(getName() + " has an education in computer sciences");
     }
 
+
+    /**
+     * all the methods in one
+     */
     @Override
     protected void all() {
         eat();
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Person
 project with the following diagram
-![MyUML](https://user-images.githubusercontent.com/67556986/88488170-a21ed980-cf9c-11ea-9044-62b4591c4acb.jpg)
+![Untitled Document](https://user-images.githubusercontent.com/67556986/88488620-6a656100-cf9f-11ea-986c-3113da501385.jpg)
